fd545bf189c30633bcdc9bd196b36727ec3a88bd
CrazyDev <95604642+ItzMeCrazy@users.noreply.github.com>
2023-01-21 21:18:05 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,17 +2,17 @@ require("dotenv").config();
 
 module.exports = {
   token: process.env.TOKEN || '', // your discord bot token
-  prefix: process.env.PREFIX || '!', // bot prefix
-  ownerID: process.env.OWNERID || ['959276033683628122', '519666024220721152'], //your discord id
-  SpotifyID: process.env.SPOTIFYID || 'cb41529dc3bd4d8f8a240dbee0fff4e8', // spotify client id
-  SpotifySecret: process.env.SPOTIFYSECRET || 'bcca82f42930498aa385a8289fdf276b', // spotify client secret
-  mongourl: process.env.MONGO_URI || 'mongodb+srv://aromaxdev:diwasatreya@youtube.rn0v74r.mongodb.net/?retryWrites=true&w=majority', // MongoDb URL
+  prefix: process.env.PREFIX || '$', // bot prefix
+  ownerID: process.env.OWNERID || ['820928134886326272'], //your discord id
+  SpotifyID: process.env.SPOTIFYID || '', // spotify client id
+  SpotifySecret: process.env.SPOTIFYSECRET || '', // spotify client secret
+  mongourl: process.env.MONGO_URI || '', // MongoDb URL
   embedColor: process.env.COlOR || '#2B2928', // embed colour
   logs: process.env.LOGS || '1026075600089256017', // Discord channel id 
   links: {
-    support: process.env.SUPPORT || 'https://aromaxdev.xyz/discord',
-    invite: process.env.INVITE || 'https://aromaxdev.xyz/discord',
-    vote: process.env.VOTE || 'https://aromaxdev.xyz/discord',
+    support: process.env.SUPPORT || 'https://discord.gg/devshub',
+    invite: process.env.INVITE || 'https://discord.gg/devshub',
+    vote: process.env.VOTE || 'https://discord.gg/devshub',
     bg: process.env.BG || 'https://media.discordapp.net/attachments/1028936089555193887/1029023341312487505/musicplayer-home.png'
   },
 
